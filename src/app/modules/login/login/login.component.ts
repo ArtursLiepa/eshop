9ced23e8b8c5fef36d5147078be7cea3e5c4551a
Arturs Liepa <arturs.liepa85@gmail.com>
2026-03-27 02:30:39 +0200

    reactive authentication form created

--- src/app/modules/login/login/login.component.ts
+++ src/app/modules/login/login/login.component.ts
@@ -1,5 +1,13 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
+import { jwtDecode } from 'jwt-decode';
+import { AuthResponse } from '../../../Interfaces/authentication';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +15,37 @@ import { AuthService } from './auth.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  @Output() loginExit = new EventEmitter();
+export class LoginComponent implements OnInit {
+  tokenExist: boolean = false;
+  token: string = '';
+
+  @Output() loginExit = new EventEmitter<boolean>();
+  @Output() userLogeIn = new EventEmitter();
 
   constructor(public authService: AuthService) {}
 
   closeloginForm() {
     this.loginExit.emit();
   }
+
+  authForm = new FormGroup({
+    username: new FormControl('', { nonNullable: true }),
+    password: new FormControl('', { nonNullable: true }),
+  });
+
+  get username() {
+    return this.authForm.controls.username;
+  }
+
+  get password() {
+    return this.authForm.controls.password;
+  }
+
+  login() {
+    this.authService
+      .authenticate(this.username.value, this.password.value)
+      .subscribe((token) => ((this.token = token), this.loginExit.emit()));
+  }
+
+  ngOnInit(): void {}
 }
